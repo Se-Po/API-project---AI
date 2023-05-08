f170f35aeb1389cf9380c2a9007bc611a8839317
SePo <sebastianpopa@live.com>
2023-05-08 18:12:49 +0300

    added Header.jsx And Footer.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
-import React from 'react'
+import { Container, Box } from '@chakra-ui/react';
+import Header from './components/Header';
+import Footer from './components/Footer';
+// import React from 'react'
 
 const App = () => {
-  return (
-    <>
-    <h1>My App</h1>
-    </>
-  )
-}
+   return (
+      <Box bg='blue.600' color='white' height='100vh' paddingTop={130}>
+         <Container maxW='3xl' centerContent>
+            <Header />
+            <Footer />
+         </Container>
+      </Box>
+   );
+};
 
-export default App
+export default App;
